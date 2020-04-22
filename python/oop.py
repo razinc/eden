595d2f99d59c63d4d55320a86a46979f851957b6
razinc <l_noir@yahoo.com>
2020-04-22 19:21:18 +0800

    Update oop.py

--- python/oop.py
+++ python/oop.py
@@ -1,6 +1,8 @@
-#########################################################
-# 1. https://www.youtube.com/watch?v=ZDa-Z5JzLYM&t=685s #
-#########################################################
+######################################################
+# Tutorial 1                                         #
+# Classes and Instances                              #
+# https://www.youtube.com/watch?v=ZDa-Z5JzLYM&t=685s #
+######################################################
 
 # attributes    - adjectives of a class
 # method        - verb of a class
@@ -8,14 +10,23 @@
 # defining class
 class Employee:
 
+    raise_amount = 1.4
+    no_of_emps = 0
+
     # constructor
     def __init__(self, firstn, lastn, pay):
         self.firstn = firstn
         self.lastn = lastn
         self.email = f"{firstn}.{lastn}@company.com"
 
+        Employee.no_of_emps += 1
+
     def full_name(self):
-        return f"{self.firstn} {self.lastn}"    
+        return f"{self.firstn} {self.lastn}"
+
+    def apply_raise(self):
+        # self.raise_amount != Employee.raise_amount
+        self_pay = int(self.pay * self.raise_amount)        
 
 emp_1 = Employee("Bruce", "Wayne", 5000)
 emp_2 = Employee("Clark", "Kent", 200)
@@ -27,3 +38,25 @@ print(emp_2.email)
 # print method
 print(emp_1.full_name())
 print(Employee.full_name(emp_2))
+
+####################################################################################################
+# Tutorial 2                                                                                       # 
+# Class Variable                                                                                   #
+# https://www.youtube.com/watch?v=BJ-VvGyQxho&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=3&t=0s #
+####################################################################################################
+
+# variable of class
+print(Employee.raise_amount)
+
+# variable of instance
+print(emp_1.raise_amount)
+
+# changing variable of instance
+emp_2.raise_amount = 1.5
+print(Employee.raise_amount)
+print(emp_1.__dict__)
+print(emp_1.raise_amount)
+print(emp_2.__dict__)
+print(emp_2.raise_amount)
+
+print(Employee.no_of_emps)
