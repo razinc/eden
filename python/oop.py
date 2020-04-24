26a0af2ef745d2bf14ea62a6eaca51a36188d112
razinc <l_noir@yahoo.com>
2020-04-24 08:29:37 +0800

    Update oop.py

--- python/oop.py
+++ python/oop.py
@@ -1,8 +1,8 @@
-######################################################
-# Tutorial 1                                         #
-# Classes and Instances                              #
-# https://www.youtube.com/watch?v=ZDa-Z5JzLYM&t=685s #
-######################################################
+####################################################################################################
+# Tutorial 1                                                                                       #
+# Classes and Instances                                                                            #
+# https://www.youtube.com/watch?v=ZDa-Z5JzLYM&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=2&t=0s #
+####################################################################################################
 
 # attributes    - adjectives of a class
 # method        - verb of a class
@@ -26,7 +26,24 @@ class Employee:
 
     def apply_raise(self):
         # self.raise_amount != Employee.raise_amount
-        self_pay = int(self.pay * self.raise_amount)        
+        self_pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    # classmethod as alternative constructor
+    @classmethod
+    def from_str(cls, new_emp):
+        firstn, lastn, pay = new_emp.split("-")
+        return cls(firstn, lastn, pay)
+
+    @staticmethod
+    def is_working_day(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        else:
+            return True
 
 emp_1 = Employee("Bruce", "Wayne", 5000)
 emp_2 = Employee("Clark", "Kent", 200)
@@ -58,5 +75,26 @@ print(emp_1.__dict__)
 print(emp_1.raise_amount)
 print(emp_2.__dict__)
 print(emp_2.raise_amount)
-
 print(Employee.no_of_emps)
+
+####################################################################################################
+# Tutorial 3                                                                                       # 
+# classmethods & staticmethods                                                                     #
+# https://www.youtube.com/watch?v=rq8cL2XMM5M&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=4&t=0s #
+####################################################################################################
+
+# regular method    - pass self
+# classmethod       - pass class
+# staticmethod      - no relation to instance or class
+
+Employee.set_raise_amt(1.05)
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
+
+emp_3 = Employee.from_str("Hal-Jordan-500")
+print(emp_3.email)
+
+import datetime
+my_date = datetime.date(2016, 7, 11)
+print(Employee.is_working_day(my_date))
